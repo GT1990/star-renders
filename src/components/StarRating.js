ce244eccdf2fd5fb279159f389aeff6d9cf739bf
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-01-25 22:04:42 -0700

    Initial commit

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -3,35 +3,20 @@ import Star from './Star';
 
 class StarRating extends Component {
 
-  // 1. Initialize a 'rating' state
-  state = {
-    rating: 0
-  };
+  // Initialize a 'rating' state
 
-  // 2. Write a function that returns 5 Star components
-  renderStars = () => {
-    let stars = [];
-    let maxRating = 5;
 
-    for (let i = 0; i < maxRating; i++) {
-      stars.push(
-        <Star 
-          key={i}
-        />
-      );
-    }
-    return stars;
-  }
+  // Write a function that returns 5 Star components
 
-  // 3. Write an event handler that updates the rating state
-  //    Pass the function to a Star component via props
+
+  // Write an event handler that updates the rating state.
+  // Pass the function to a Star component via props
 
 
   render() {
     return (
       <ul className="course--stars">
-        {/* 4. Render the Star components */}
-        { this.renderStars() }
+        {/* Render the Star components */}
       </ul>
     );
   }
